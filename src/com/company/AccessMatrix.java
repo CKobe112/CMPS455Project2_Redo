dc5986ea34ee45ad5d6c806b4c603a3e87f953c7
Austin <c00010908@louisiana.edu>
2022-03-07 12:38:54 -0600

    added some more functionality to AccesMatrix.java

--- src/com/company/AccessMatrix.java
+++ src/com/company/AccessMatrix.java
@@ -1,22 +1,25 @@
 package com.company;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Construct an access matrix with permission for each object based on the domain
  */
 public class AccessMatrix extends Thread {
 
-
     static int domainRange;
     static int objectsRange;
     static int domain;
     static int column;
     static String[][] matrix;
-    static Lock lock;
+    static Lock lock = new ReentrantLock();
     static Random random = new Random();
     static String[] charArray;
+    static final int threadNum = ThreadLocalRandom.current().nextInt(3,8);
+    int yield = ThreadLocalRandom.current().nextInt(3,8);
 
     public static String randomDomain(){
         int r = random.nextInt(2);
@@ -40,6 +43,9 @@ public class AccessMatrix extends Thread {
         }
 
     }
+    public static String threadName(){
+        return Thread.currentThread().getName();
+    }
 
 
 
@@ -48,7 +54,7 @@ public class AccessMatrix extends Thread {
     public void read(){
         //need to change to actual file number, but this is a temp solution
         int charPosition = random.nextInt(objectsRange);
-        System.out.println(charArray[charPosition]);
+        System.out.println(Thread.currentThread().getName() + " resource contains " + charArray[(int) Thread.currentThread().getId()]);
     }
     public String write(){
         int randomString = random.nextInt(6);
@@ -71,6 +77,10 @@ public class AccessMatrix extends Thread {
             return "Rainbow";
         }
     }
+    public String setMatrixPosition(int rows, int columns){
+        return matrix[rows][columns];
+    }
+
 
 
     //getters and setters, probably wont need most of these
