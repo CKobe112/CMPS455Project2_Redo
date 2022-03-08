fd47d789ddeee0c2cf88562a2894dd849cbc1477
Austin <c00010908@louisiana.edu>
2022-03-08 12:54:38 -0600

    checking for changes

--- src/com/company/MatrixInit.java
+++ src/com/company/MatrixInit.java
@@ -10,7 +10,6 @@ class MatrixInit extends AccessMatrix {
         charArray = new String[objectsRange];
         matrix = new String[domainRange][objectsRange + domainRange];
         lock = new ReentrantLock[objectsRange];
-        //final int threadNum = ThreadLocalRandom.current().nextInt(3,8);
         for (int i = 0; i < objectsRange;i++) {
             lock[i] = new ReentrantLock();
         }
@@ -20,6 +19,29 @@ class MatrixInit extends AccessMatrix {
          */
         for (int i = 0; i < objectsRange; i++) {
             charArray[i] = writeArray();
+        }
+
+
+        /**
+         * initialize 2D array
+         */
+        for (int i = 0; i < domainRange; i++) {
+            for (int j = 0; j < domainRange + objectsRange; j++) {
+                matrix[i][j] = "";
+            }
+        }
+
+        /**
+         * set random permissions and randomly allow for switching
+         */
+        for (int i = 0; i < domainRange; i++) {
+            for (int j = 0; j < domainRange+objectsRange; j++) {
+                if (j >= objectsRange) {
+                    matrix[i][j] = randomDomain();
+                } else {
+                    matrix[i][j] = randomPermission();
+                }
+            }
         }
         /**
          * The header for the 2D array
@@ -34,78 +56,6 @@ class MatrixInit extends AccessMatrix {
             System.out.print("D" + i + " | ");
         }
         System.out.println("    ");
-
-        /**
-         * initialize 2D array
-         */
-        for (int i = 0; i < domainRange; i++) {
-            for (int j = 0; j < domainRange + objectsRange; j++) {
-                matrix[i][j] = "";
-            }
-        }
-
-        /**
-         * sets the array to R or W or R/W or allow randomly
-         */
-        /*for (int i = 0; i < domain; i++) {
-            for (int j = 0; j < domain + objects; j++) {
-                switch (permissions) {
-                    case 1:
-                        for (int i = 0; i < domain; i++) {
-                            for (int j = 0; j < domain + objects; j++) {
-                                matrix[i][j] = "R";
-                            }
-                        }
-                        break;
-                    case 2:
-                        for (int i = 0; i < domain; i++) {
-                            for (int j = 0; j < domain + objects; j++) {
-                                matrix[i][j] = "W";
-                            }
-                        }
-                        break;
-                    case 3:
-                        for (int i = 0; i < domain; i++) {
-                            for (int j = 0; j < domain + objects; j++) {
-                                matrix[i][permissions] = "R";
-                            }
-                        }
-                        break;
-                    case 4:
-                        for (int i = 0; i < domain; i++) {
-                            for (int j = 0; j < domain + objects; j++) {
-                                matrix[i][permissions] = "R";
-                            }
-                        }
-                        break;
-                    default:
-                        for (int i = 0; i < domain; i++) {
-                            for (int j = 0; j < domain + objects; j++) {
-                                matrix[i][permissions] = "R";
-                            }
-                        }
-
-                }
-            }
-        }*/
-        /**
-         * sets the first column equal to the domain header
-         */
-//        for (int i = 0; i < domain; i++) {
-//            matrix[i][0] = "D" + i;
-//        }
-        /**
-         * set random permissions and randomly allow for switching
-         */
-        for (int i = 0; i < domainRange; i++) {
-            for (int j = 0; j < domainRange+objectsRange; j++) {
-                if (j >= objectsRange) {
-                    matrix[i][j] = randomDomain();
-                } else {
-                    matrix[i][j] = randomPermission();
-                }
-            }
-        }
         /**
          * prints the matrix
          */
@@ -116,29 +66,12 @@ class MatrixInit extends AccessMatrix {
             }
             System.out.println();
         }
-//        for (int i = 0; i < objects; i++) {
-//            System.out.println(charArray[i]);
-//        }
     }
-
     @Override
     public void run() {
-        //int randomRow = 1 + (int) (Math.random() * ((7 - 3) + 1));
-        //int randomColumn = 1 + (int) (Math.random() * ((7 - 3) + 1));
         for (int i = 0; i < domainRange; i++) {
             MatrixThread threads = new MatrixThread(i);
             threads.start();
         }
-
-            //System.out.println();
-            //System.out.println(randomRow);
-            //System.out.println(randomColumn);
-//        for (int i = 0; i < yield; i++) {
-//            AccessMatrix.yield();
-//        }
-
-
-
-
     }
 }
